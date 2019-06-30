79577a9580f064cec4c54ded130c9ef23299fd05
William Montout <williammontout@williams-mbp.home>
2019-07-01 00:02:19 +0100

    no comment

--- William's code/Home-page/burger-animation.js
+++ William's code/Home-page/burger-animation.js
@@ -1,5 +1,9 @@
-
-
-function myFunction(animate) {
-    x.classList.toggle("change");
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
   }
